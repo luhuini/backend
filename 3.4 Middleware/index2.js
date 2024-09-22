847fff5848ab22451d665b51738b9cb5f7e7c398
Lu <stupidlifewww@foxmail.com>
2024-09-22 23:10:34 +0100

    custom middlerware

--- 3.4 Middleware/index2.js
+++ 3.4 Middleware/index2.js
@@ -1,8 +1,14 @@
 import express from "express";
-
+import morgan from "morgan";
+/* Create a new morgan logger middleware function using the given format and options. 
+The format argument may be a string of a predefined name (see below for the names),
+ a string of a format string, 
+ or a function that will produce a log entry. */
 const app = express();
 const port = 3000;
-
+// Use morgan for logging requests
+app.use(morgan("short")); // 'dev' "combined" is a predefined format
+// result:  ::1 - - [22/Sep/2024:21:15:17 +0000] "GET / HTTP/1.1" 200 5 "-" "PostmanRuntime/7.42.0"
 app.get("/", (req, res) => {
   res.send("Hello");
 });
--- 3.4 Middleware/index3.js
+++ 3.4 Middleware/index3.js
@@ -1,8 +1,16 @@
 import express from "express";
-
+import morgan from "morgan";
 const app = express();
 const port = 3000;
 
+// 自定义中间件
+const logger = (req, res, next) => {
+  // 记录请求的 URL
+  console.log(`req.originalUrl":${req.originalUrl}`);
+  console.log(req.method);
+  // 使用 morgan 记录请求
+  next();
+};
 app.use(logger);
 
 app.get("/", (req, res) => {
