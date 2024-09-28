6c2636d110d1ddbd9714da407fc58b7e223981e2
Lu <stupidlifewww@foxmail.com>
2024-09-28 23:57:17 +0100

    passing data client n server

--- 4.2+Passing+Data/index.js
+++ 4.2+Passing+Data/index.js
@@ -3,12 +3,20 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
-
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  console.log("hah");
+  res.render("index.ejs", { length: 0 });
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  let fullName = req.body["fName"] + req.body["lName"];
+  let fullNameLen = fullName.length;
+  console.log(fullNameLen);
+  res.render("index.ejs", { length: fullNameLen });
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
